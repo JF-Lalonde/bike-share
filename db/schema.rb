1c69c455aef09d0e623023f903a5c204ec0b2b5d
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-30 18:31:17 -0600

    city removed from stations table, replaced with city_id referencing city table. Still need to ensure 'has many'/'belongs to' relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701001532) do
+ActiveRecord::Schema.define(version: 20170701002934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,10 @@ ActiveRecord::Schema.define(version: 20170701001532) do
     t.float    "lat"
     t.float    "long"
     t.integer  "dock_count"
-    t.string   "city"
     t.date     "installation_date"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "city_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,35 @@ require 'Date'
 require 'active_support/core_ext'
 
 
-CSV.foreach("./db/csv/station.csv", :headers => true) do |row|
+# CSV.foreach("./db/csv/station.csv", :headers => true) do |row|
+#
+#  city_name = row["city"]
+#   date = row["installation_date"]
+#
+#  row.delete("id")
+#
+#  row['installation_date'] = Date.strptime(date, '%m/%d/%Y')
+#   Station.create!(row.to_h) unless Station.exists?(name: row['name'])
+# end
+#
+# Station.uniq.pluck(:city).each do |city|
+#   City.create!(name: city) unless City.exists?(name: row['name'])
+# end
 
- city_name = row["city"]
-  date = row["installation_date"]
-
- row.delete("id")
-
- row['installation_date'] = Date.strptime(date, '%m/%d/%Y')
-  Station.create!(row.to_h) unless Station.exists?(name: row['name'])
+Station.where(city: "Palo Alto").each do |station|
+  station.update_attributes(city_id: City.find_by(name: "Palo Alto").id)
 end
 
-Station.uniq.pluck(:city).each do |city|
-  City.create!(name: city)
+Station.where(city: "San Francisco").each do |station|
+  station.update_attributes(city_id: City.find_by(name: "San Francisco").id)
+end
+
+Station.where(city: "San Jose").each do |station|
+  station.update_attributes(city_id: City.find_by(name: "San Jose").id)
+end
+
+Station.where(city: "Mountain View").each do |station|
+  station.update_attributes(city_id: City.find_by(name: "Mountain View").id)
 end
 
 puts "station database seeded"
