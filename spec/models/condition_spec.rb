acdf17ed1816e141e78db7f615cabe39f891f38a
wratterman <will.ratterman@gmail.com>
2017-07-03 13:18:08 -0600

    inital test writted for condition model

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -1,3 +1,12 @@
 RSpec.decscribe Condition do
+  describe "Validations" do
+    it "is valid with date, max & min temps, mean humidity, visibility, wind speed, and precipitation" do
+      cond = Condition.create(date: , max_temperature_f: , min_temperature_f: ,
+                              mean_temperature_f:, mean_humidity: ,
+                              mean_visibility_miles: ,mean_wind_speed_mph: ,
+                              precipitation_inches: )
 
+      expect(cond).to be_valid
+    end
+  end
 end
