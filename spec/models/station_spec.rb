37ef4c43169e2d992899a90bbbc004d3bd3a5dc4
Michael Centrelli <Acentrelli@gmail.com>
2017-07-02 18:21:27 -0600

    Station analytics tests

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -39,4 +39,30 @@ RSpec.describe Station do
       expect(st).to respond_to(:installation_date)
     end
   end
+
+  describe 'Analytics' do
+    it "will return average bikes per station" do
+      st = Station.create(name: "Penn Station", dock_count: 32, city_id: 4, installation_date: 'Sat, 08 Jun 2013')
+      st_2 = Station.create(name: "Need a gearup", dock_count: 62, city_id: 3, installation_date: 'Sat, 08 Jun 2013')
+      avg_sts = Station.avg_bikes_per_station
+
+      expect(avg_sts).to eq(47)
+    end
+
+    it "will return the most bikes" do
+      st = Station.create(name: "Penn Station", dock_count: 32, city_id: 4, installation_date: 'Sat, 08 Jun 2013')
+      st_2 = Station.create(name: "Need a gearup", dock_count: 62, city_id: 3, installation_date: 'Sat, 08 Jun 2013')
+      most_bikes = Station.most_bikes
+
+      expect(most_bikes).to eq(62)
+    end
+
+    it "will return the station(s) with the most bikes" do
+      st = Station.create(name: "Penn Station", dock_count: 32, city_id: 4, installation_date: 'Sat, 08 Jun 2013')
+      st_2 = Station.create(name: "Need a gearup", dock_count: 62, city_id: 3, installation_date: 'Sat, 08 Jun 2013')
+      result = Station.stations_with_most_bikes
+
+      expect(result[0]).to eq(st_2)
+    end
+  end
 end
