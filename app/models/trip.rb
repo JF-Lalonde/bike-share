db1f1e9284043730c7e511e6613ca550b68c3544
Michael Centrelli <Acentrelli@gmail.com>
2017-07-03 12:25:41 -0600

    monthly rides

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,8 @@
 require 'will_paginate'
 require 'will_paginate/active_record'
+require './app/models/station.rb'
+require 'Date'
+
 
 class Trip < ActiveRecord::Base
   validates :duration, presence: true
@@ -27,6 +30,20 @@ class Trip < ActiveRecord::Base
 
   def self.shortest_ride
     (Trip.pluck(:duration).min)
+  end
+
+  ## Flesh out relationship with station
+  def self.station_with_most_start_trips
+    station_name = (Trip.pluck(:start_station_name).max)
+    # Station.find_by(name: station_name).class
+  end
+
+  def self.station_with_most_end_trips
+    (Trip.pluck(:end_station_name).max)
+  end
+
+  def self.monthly_rides_breakdown(month)
+    Trip.where('extract(month from start_date) =?',month)
   end
 
 end
