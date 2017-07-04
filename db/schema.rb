a372f5f4f8ebb6596ed5ae3cfa237384083ac7ef
wratterman <will.ratterman@gmail.com>
2017-07-04 10:44:39 -0600

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,35 @@ ActiveRecord::Schema.define(version: 20170703203450) do
     t.string "name"
   end
 
+  create_table "conditions", force: :cascade do |t|
+    t.date     "this_date"
+    t.float    "max_temperature_f"
+    t.float    "mean_temperature_f"
+    t.float    "min_temperature_f"
+    t.float    "max_dew_point_f"
+    t.float    "mean_dew_point_f"
+    t.float    "min_dew_point_f"
+    t.float    "max_humidity"
+    t.float    "mean_humidity"
+    t.float    "min_humidity"
+    t.float    "max_sea_level_pressure_inches"
+    t.float    "mean_sea_level_pressure_inches"
+    t.float    "min_sea_level_pressure_inches"
+    t.float    "max_visibility_miles"
+    t.float    "mean_visibility_miles"
+    t.float    "min_visibility_miles"
+    t.float    "max_wind_Speed_mph"
+    t.float    "mean_wind_speed_mph"
+    t.float    "max_gust_speed_mph"
+    t.float    "precipitation_inches"
+    t.float    "cloud_cover"
+    t.string   "events"
+    t.float    "wind_dir_degrees"
+    t.string   "zipcode"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
+
   create_table "end_stations", force: :cascade do |t|
     t.integer "station_id"
   end
